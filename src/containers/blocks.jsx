af72128ce9b1c183a137a5987b27ff940a946a13
adroitwhiz <adroitwhiz@protonmail.com>
2023-03-14 01:17:17 -0400

    refactor: use vm.editingTarget
    I'll be removing runtime.getEditingTarget soon.

--- src/containers/blocks.jsx
+++ src/containers/blocks.jsx
@@ -547,7 +547,7 @@ class Blocks extends React.Component {
                 {this.state.prompt ? (
                     <Prompt
                         defaultValue={this.state.prompt.defaultValue}
-                        isStage={vm.runtime.getEditingTarget().isStage}
+                        isStage={vm.editingTarget.isStage}
                         showListMessage={this.state.prompt.varType === this.ScratchBlocks.LIST_VARIABLE_TYPE}
                         label={this.state.prompt.message}
                         showCloudOption={this.state.prompt.showCloudOption}
